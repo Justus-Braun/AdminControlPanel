db1b99e85ed6f271ea60358cbb0563aae89f6a62
Kieberts <65255153+Kieberts@users.noreply.github.com>
2022-01-24 23:11:58 +0100

    Login in designed, Calling the Data does NOT work yet!

--- ClientApp/src/components/LoginForm.js
+++ ClientApp/src/components/LoginForm.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
-import auth from "./../auth";
+import React, {useState} from 'react';
+import auth from './../auth';
 
 
-function CheckUser(obj) {
+
+async function CheckUser(obj) {
+    let fetchDomain = 'api/admins/all';
+    const response = await fetch(fetchDomain);
+    const data = await response.json();
+    
+    console.log("Domain: " + fetchDomain);
+    console.log("response: " + response);
+    console.log("Row: " + data);
+    console.log("Index: " + data[0]);
+    
     if (obj.name === "tom") {
         return true;
     }
@@ -12,56 +22,63 @@ function CheckUser(obj) {
     
 }
 
-export  const LoginForm = props => {
+export  const LoginForm =  props => {
     
+
     const [user, setUser] = useState({name: "", password: ""});
 
-    
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        
-        
+
+
         if (CheckUser(user)) {
             alert(`logged In: ${user.name}`);
-            /*
+
+            
             auth.login(() => {
                 props.history.push("/home");
             });
-            */
-        }
-        else {
+            
+        } else {
             alert(`Error: ${user.name}`);
         }
-        
+
     }
     return (
         <>
-            
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label>Enter your name:
-                            <input
-                                type="text"
-                                value={user.name}
-                                onChange={(e) => setUser({...user, name: e.target.value})}
-                            />
-                        </label>
-                    </div>
-                    <div>
-                        <label>Enter your password:
-                            <input
-                                type="text"
-                                value={user.password}
-                                onChange={(e) => setUser({...user, password: e.target.value})}
-                            />
-                        </label>
-                    </div>
-                    <input type="submit"/>
-                </form>
-            
+
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Enter your name:
+                        <input
+                            type="text"
+                            value={user.name}
+                            onChange={(e) => setUser({...user, name: e.target.value})}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Enter your password:
+                        <input
+                            type="text"
+                            value={user.password}
+                            onChange={(e) => setUser({...user, password: e.target.value})}
+                        />
+                    </label>
+                </div>
+                <input type="submit"/>
+            </form>
+
         </>
-        
-    )    
+
+    )
+
 }
+
+
+
+
+
 
 

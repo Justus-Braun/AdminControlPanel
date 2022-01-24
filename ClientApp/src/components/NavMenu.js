e80173073e04f07dae23141daae129268dd70ab3
Kieberts <65255153+Kieberts@users.noreply.github.com>
2022-01-24 23:11:30 +0100

    .

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import {Button, Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
